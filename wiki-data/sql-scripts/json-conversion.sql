4dc321adcfaee1f607b181adaf965a40b8ba2e15
gfenelus <gfenelus@student.neumont.edu>
2023-02-27 19:14:20 -0700

    full deity list + updated script

--- wiki-data/sql-scripts/json-conversion.sql
+++ wiki-data/sql-scripts/json-conversion.sql
@@ -1,11 +1,11 @@
 use deities
 
-select * from openrowset (bulk 'C:\wikipedia-project\wiki-data\deity-profiles\greek-gods.json', Single_CLOB) as
+select * from openrowset (bulk 'C:\wikipedia-project\wiki-data\deity-profiles\deities.json', Single_CLOB) as
 imporrt;
 
 Declare @JSON varchar(max)
 SELECT @JSON=BulkColumn
-FROM OPENROWSET (BULK 'C:\wikipedia-project\wiki-data\deity-profiles\greek-gods.json', SINGLE_CLOB) import
+FROM OPENROWSET (BULK 'C:\wikipedia-project\wiki-data\deity-profiles\deities.json', SINGLE_CLOB) import
 SELECT * FROM OPENJSON (@JSON)
 WITH  (
    [name] varchar(20),  
