f91ba7c7db366ebe1d7cd1a3db5daf318b8073eb
gfenelus <gfenelus@student.neumont.edu>
2023-03-02 17:00:05 -0700

    added type column into sql import script

--- wiki-data/sql-scripts/json-conversion.sql
+++ wiki-data/sql-scripts/json-conversion.sql
@@ -8,6 +8,7 @@ SELECT @JSON=BulkColumn
 FROM OPENROWSET (BULK 'C:\wikipedia-project\wiki-data\deity-profiles\deities.json', SINGLE_CLOB) import
 SELECT * INTO deity_table FROM OPENJSON (@JSON)
 WITH  (
+   [type] varchar(20),
    [name] varchar(20),  
    [god] varchar(max),  
    [abode] varchar(max),
@@ -16,4 +17,14 @@ WITH  (
    [sibling] varchar(max),
    [children] varchar(max));
 
-   Select * from deity_table;
+   Select *  from deity_table;
+
+   /* All of Zeus's children */
+
+Select * from deity_table
+Where parent like '%Zeus%'
+
+/* all Dieties abiding in the underworld that have the parent Eris*/ 
+
+Select * from deity_table
+where abode like '%underworld%' and parent like '%Eris%'
